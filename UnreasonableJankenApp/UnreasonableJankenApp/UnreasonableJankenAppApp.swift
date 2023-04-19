fdd41e509ff06684fe98f9ad240c7e0216af6ac3
KobayashiYoh <y.kobayashi@4nonome.com>
2023-04-19 22:42:13 +0900

    create project

---
+++ UnreasonableJankenApp/UnreasonableJankenApp/UnreasonableJankenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnreasonableJankenAppApp.swift
+//  UnreasonableJankenApp
+//
+//  Created by 小林陽 on 2023/04/19.
+//
+
+import SwiftUI
+
+@main
+struct UnreasonableJankenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
