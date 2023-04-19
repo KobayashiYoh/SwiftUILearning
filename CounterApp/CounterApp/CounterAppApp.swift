24ed906148e9a42b70dc15468d60f34efc99b412
KobayashiYoh <y.kobayashi@4nonome.com>
2023-04-19 22:00:32 +0900

    create project

---
+++ CounterApp/CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by 小林陽 on 2023/04/19.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
