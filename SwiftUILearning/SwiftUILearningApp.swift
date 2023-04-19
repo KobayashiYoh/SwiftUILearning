8c1ade993b776eae2fef23911f0895224cd0752c
KobayashiYoh <y.kobayashi@4nonome.com>
2023-04-19 21:52:47 +0900

    first commit

---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by 小林陽 on 2023/04/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
