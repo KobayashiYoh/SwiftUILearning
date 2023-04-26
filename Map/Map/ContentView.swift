ffc0626fe27e18074ba3c18a5edf4e07d46980bf
KobayashiYoh <y.kobayashi@4nonome.com>
2023-04-26 22:05:26 +0900

    MapKitで地図を表示

--- Map/Map/ContentView.swift
+++ Map/Map/ContentView.swift
@@ -6,16 +6,19 @@
 //
 
 import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        return MKMapView()
+    }
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        MapView().ignoresSafeArea()
     }
 }
 
