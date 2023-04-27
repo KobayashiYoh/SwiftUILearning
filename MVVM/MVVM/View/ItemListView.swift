2981dc5a56c203d22b91bc082db7dfa05aef5d3b
KobayashiYoh <y.kobayashi@4nonome.com>
2023-04-27 23:57:56 +0900

    アーキテクチャに応じたディレクトリを作成

--- MVVM/MVVM/View/ItemListView.swift
+++ MVVM/MVVM/View/ItemListView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  ItemListView.swift
 //  MVVM
 //
 //  Created by 小林陽 on 2023/04/27.
@@ -7,28 +7,6 @@
 
 import SwiftUI
 
-// Model
-struct Item: Identifiable {
-    var id = UUID()
-    var name: String
-    var description: String
-}
-
-// ViewModel
-class ItemListViewModel: ObservableObject {
-    @Published var items = [
-        Item(name: "Item 1", description: "This is the first item."),
-        Item(name: "Item 2", description: "This is the second item."),
-        Item(name: "Item 3", description: "This is the third item.")
-    ]
-    
-    func addItem(name: String, description: String) {
-        let newItem = Item(name: name, description: description)
-        items.append(newItem)
-    }
-}
-
-// View
 struct ItemListView: View {
     @ObservedObject var viewModel = ItemListViewModel()
     @State var newItemName = ""
@@ -59,15 +37,8 @@ struct ItemListView: View {
     }
 }
 
-
-struct ContentView: View {
-    var body: some View {
-        ItemListView()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
+struct ItemListView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ItemListView()
     }
 }
