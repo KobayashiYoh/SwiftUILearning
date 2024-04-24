06e74b9e80275b9aca872f095eab6cc5ef06f7e5
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-04-24 21:41:32 +0900

    create JankenApp

---
+++ JankenApp/JankenApp/JankenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JankenAppApp.swift
+//  JankenApp
+//
+//  Created by 小林陽 on 2024/04/24.
+//
+
+import SwiftUI
+
+@main
+struct JankenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
