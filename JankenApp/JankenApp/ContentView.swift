193a2d877680b60af09b7984405b3640ae492a31
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-04-25 20:38:11 +0900

    じゃんけんの勝敗のロジックを作成

--- JankenApp/JankenApp/ContentView.swift
+++ JankenApp/JankenApp/ContentView.swift
@@ -8,23 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        HStack {
-            Spacer()
-            HandButton(onTap: {
-                print(Hand.rock.text)
-            }, hand: .rock)
-            Spacer()
-            HandButton(onTap: {
-                print(Hand.paper.text)
-            }, hand: .scissors)
-            Spacer()
-            HandButton(onTap: {
-                print(Hand.scissors.text)
-            }, hand: .paper)
-            Spacer()
+    @State private var message = "じゃんけん…"
+    @State private var cpuHand = Hand.rock
+
+    func generateCPUHand() -> Hand {
+        let hands = [Hand.rock, Hand.scissors, Hand.paper]
+        let hand = hands.randomElement()
+        return hand!
+    }
+
+    func judgeResultMessage(playerHand: Hand, cpuHand: Hand) -> String {
+        let isDraw = playerHand == cpuHand
+        let isWin = (playerHand == .rock && playerHand == .scissors) || (playerHand == .scissors && cpuHand == .paper) || (playerHand == .paper && cpuHand == .rock)
+        if (isDraw) {
+            return "引き分け"
+        } else if (isWin) {
+            return "勝ち"
+        } else {
+            return "負け"
         }
-        .padding()
+    }
+
+    func onTapHand(hand: Hand) -> Void {
+        let playerHand = hand
+        cpuHand = generateCPUHand()
+        message = judgeResultMessage(playerHand: playerHand, cpuHand: cpuHand)
+    }
+
+    var body: some View {
+        VStack {
+            Spacer()
+            Text(message).font(.title)
+            Text("😎" + cpuHand.emoji)
+            Spacer()
+            HStack {
+                Spacer()
+                HandButton(onTap: {
+                    onTapHand(hand: .rock)
+                }, hand: .rock)
+                Spacer()
+                HandButton(onTap: {
+                    onTapHand(hand: .scissors)
+                }, hand: .scissors)
+                Spacer()
+                HandButton(onTap: {
+                    onTapHand(hand: .paper)
+                }, hand: .paper)
+                Spacer()
+            }
+            .padding()
+        }
     }
 }
 
